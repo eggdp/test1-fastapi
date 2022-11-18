317ff44a1b4dbcb52032a779c3c487ab349e0ceb
eggdp <pur9799@gmail.com>
2022-11-18 14:28:58 +0900

    에러처리dto

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 
+class ResponseDTO(BaseModel):
+    code: int
+    message:str
+    data: object
+    
+    
 class Cat(BaseModel):
     name : str
     id : int =0
@@ -20,3 +28,17 @@ async def second(skip:int =0,limit:int=10):
 @app.post("/cat")
 async def cat(cat: Cat):
     return cat
+
+
+@app.get("/error")
+async def error():
+    dto=ResponseDTO(
+        code=1,
+        message="aa",
+        data=None
+    )
+    return JSONResponse(status_code=404, content=jsonable_encoder(dto))
+
+@app.get("/error1")
+async def error1():
+    return HTTPException(status_code=404, detail={"Message":"Item not found1"})
