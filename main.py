66262f647d4d0dbac1a3fa0cdcbca11c38354917
eggdp <pur9799@gmail.com>
2022-11-18 11:30:12 +0900

    path/ query / body

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Cat(BaseModel):
+    name : str
+    id : int =0
 
 app = FastAPI()
 
 
-@app.get("/first")
-async def root():
-    return "hi "
+@app.get("/first/{id}")
+async def root(id : int):
+    return {"message":"hello" , "id" : id}
+
+@app.get("/second")
+async def second(skip:int =0,limit:int=10):
+    return {"skip":skip,"limit":limit}
+
+
+@app.post("/cat")
+async def cat(cat: Cat):
+    return cat
